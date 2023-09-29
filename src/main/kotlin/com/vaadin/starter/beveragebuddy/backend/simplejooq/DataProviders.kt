1853e8a93ee13c3dd503ab29bb9b9186a8fbe3a3
Martin Vysny <mavi@vaadin.com>
2023-09-29 16:12:19 +0300

    Remove unused stuff

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/simplejooq/DataProviders.kt
+++
@@ -1,6 +0,0 @@
-package com.vaadin.starter.beveragebuddy.backend.simplejooq
-
-import com.vaadin.starter.beveragebuddy.ui.AbstractJooqDataProvider
-import org.jooq.Condition
-import org.jooq.Record
-
