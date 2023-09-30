a87967d154eae46d5092e9f7824c01c28b2d1a0a
Martin Vysny <mavi@vaadin.com>
2023-09-30 09:08:45 +0300

    minor

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/ui/categories/CategoryDataProvider.kt
+++ src/main/kotlin/com/vaadin/starter/beveragebuddy/ui/categories/CategoryDataProvider.kt
@@ -13,10 +13,10 @@ import java.io.Serializable
 import java.util.stream.Stream
 
 class CategoryDataProvider :
-    AbstractJooqDataProvider<CategoryRow, String>(CATEGORY), ConfigurableFilterDataProvider<CategoryRow, String, String> {
+    AbstractJooqDataProvider<CategoryRow, Void>(CATEGORY), ConfigurableFilterDataProvider<CategoryRow, Void, String> {
 
-    private fun getWhereClause(query: Query<CategoryRow, String>): Condition {
-        val f = (query.filter.orElse(null) ?: filter).trim()
+    private fun getWhereClause(): Condition {
+        val f = filter.trim()
         return if (f.isEmpty()) {
             DSL.trueCondition()
         } else {
@@ -24,11 +24,11 @@ class CategoryDataProvider :
         }
     }
 
-    override fun fetchFromBackEnd(query: Query<CategoryRow, String>): Stream<CategoryRow> {
+    override fun fetchFromBackEnd(query: Query<CategoryRow, Void>): Stream<CategoryRow> {
         val result: List<CategoryRow> = db {
             create.select(CATEGORY.asterisk(), reviewCountField)
                 .from(CATEGORY)
-                .where(getWhereClause(query))
+                .where(getWhereClause())
                 .orderByQuery(query)
                 .limit(query.limit)
                 .offset(query.offset)
@@ -37,10 +37,10 @@ class CategoryDataProvider :
         return result.stream()
     }
 
-    override fun sizeInBackEnd(query: Query<CategoryRow, String>): Int = db {
+    override fun sizeInBackEnd(query: Query<CategoryRow, Void>): Int = db {
         create.selectCount()
             .from(CATEGORY)
-            .where(getWhereClause(query))
+            .where(getWhereClause())
             .fetchOneInt()
     }
 
@@ -61,6 +61,10 @@ class CategoryDataProvider :
     }
 }
 
+/**
+ * Represents a [CategoryRecord] with additional information. Handy to avoid the N+1
+ * select problem - instead we'll fetch everything with one SQL SELECT command.
+ */
 data class CategoryRow(
     val category: CategoryRecord,
     val reviewCount: Int
