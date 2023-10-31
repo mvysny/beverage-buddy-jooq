2ce4a3fc422002599ea693310b02d83e187c4b47
Martin Vysny <mavi@vaadin.com>
2023-10-31 11:19:58 +0200

    minor

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
+++ src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
@@ -1,6 +1,7 @@
 package com.vaadin.starter.beveragebuddy.backend
 
 import com.fatboyindustrial.gsonjavatime.Converters
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.vaadin.starter.beveragebuddy.backend.jooq.tables.references.CATEGORY
 import com.vaadin.starter.beveragebuddy.backend.simplejooq.db
@@ -18,9 +19,10 @@ import org.http4k.routing.routes
 import org.http4k.servlet.jakarta.Http4kJakartaServletAdapter
 
 object RestService {
-    val gson = GsonBuilder().registerJavaTimeAdapters().create()
-    private fun Response.json(json: Any): Response = header("Content-Type", ContentType.APPLICATION_JSON.toHeaderValue())
-        .body(gson.toJson(json))
+    val gson: Gson = GsonBuilder().registerJavaTimeAdapters().create()
+    private fun Response.json(json: Any): Response =
+        header("Content-Type", ContentType.APPLICATION_JSON.toHeaderValue())
+            .body(gson.toJson(json))
 
     val app: RoutingHttpHandler = routes(
         "categories" bind GET to { Response(OK).json(db { CATEGORY.dao.findAll() }) }
@@ -30,10 +32,15 @@ object RestService {
 /**
  * Provides access to person list. To test, just run `curl http://localhost:8080/rest/categories`
  */
-@WebServlet(urlPatterns = ["/rest/*"], name = "RestServlet", asyncSupported = false)
+@WebServlet(
+    urlPatterns = ["/rest/*"],
+    name = "RestServlet",
+    asyncSupported = false
+)
 class RestServlet : HttpServlet() {
     private val adapter = Http4kJakartaServletAdapter(RestService.app)
-    override fun service(req: HttpServletRequest, resp: HttpServletResponse) = adapter.handle(req, resp)
+    override fun service(req: HttpServletRequest, resp: HttpServletResponse) =
+        adapter.handle(req, resp)
 }
 
 private fun GsonBuilder.registerJavaTimeAdapters(): GsonBuilder = apply {
