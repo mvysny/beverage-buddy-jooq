0773844979de15dcae125507c4a14d144bf17c69
Martin Vysny <mavi@vaadin.com>
2023-10-31 14:29:28 +0200

    minor

--- src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/GsonUtils.kt
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/GsonUtils.kt
@@ -22,16 +22,16 @@ fun <T> Gson.fromJsonArray(reader: Reader, itemClass: Class<T>): List<T> {
 }
 
 /**
- * Parses the response as a JSON and converts it to a Java object.
+ * Parses the response as a JSON and converts it to a Java object using [gson].
  */
 fun <T> Body.json(gson: Gson, clazz: Class<T>): T = gson.fromJson(stream.buffered().reader(), clazz)
 
 /**
- * Parses the response as a JSON array and converts it into a list of Java object with given [clazz] using [HttpClientVokPlugin.gson].
+ * Parses the response as a JSON array and converts it into a list of Java object with given [clazz] using [gson].
  */
 fun <T> Body.jsonArray(gson: Gson, clazz: Class<T>): List<T> = gson.fromJsonArray(stream.buffered().reader(), clazz)
 
 /**
- * Parses the response as a JSON array and converts it into a list of Java object with given [clazz] using [HttpClientVokPlugin.gson].
+ * Parses the response as a JSON array and converts it into a list of Java object with given type [T] using [gson].
  */
 inline fun <reified T> Body.jsonArray(gson: Gson): List<T> = jsonArray(gson, T::class.java)
