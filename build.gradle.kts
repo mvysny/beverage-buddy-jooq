a61b79fe927ba7089b03873dc3aebd963a600e3e
Martin Vysny <mavi@vaadin.com>
2025-01-31 08:20:09 +0200

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Original project: https://github.com/vaadin/beverage-starter-flow
 
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
     application
     alias(libs.plugins.vaadin)
 }
